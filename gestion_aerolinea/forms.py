0750cbd7e82332d0c5d56c258c855ba6c02fe1ee
octaviovictorio <octavionvictorio@gmail.com>
2025-08-07 19:19:05 -0300

    agregamos PasajeroForm y modificamos ReservaForm

--- gestion_aerolinea/forms.py
+++ gestion_aerolinea/forms.py
@@ -1,16 +1,35 @@
 from django import forms
-from .models import Reserva
+from .models import Pasajero, Reserva
+
 
 class ReservaForm(forms.ModelForm):
     class Meta:
         model = Reserva
-        fields = '__all__'
-
+        fields = ['pasajero', 'asiento'] 
     def clean(self):
         cleaned_data = super().clean()
         asiento = cleaned_data.get('asiento')
+        
+        if not asiento:
+            raise forms.ValidationError('Debes seleccionar un asiento.')
 
-        if asiento and asiento.estado != 'disponible':
-            raise forms.ValidationError("Ese asiento no está disponible.")
-
+        if asiento.estado != 'disponible':
+            raise forms.ValidationError('El asiento seleccionado no está disponible.')
         return cleaned_data
+
+
+class PasajeroForm(forms.ModelForm):
+    class Meta:
+        model = Pasajero
+        fields = ['nombre', 'apellido', 'tipo_documento', 'numero_documento', 'fecha_nacimiento', 'email', 'telefono']
+        widgets = {
+            'fecha_nacimiento': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'apellido': forms.TextInput(attrs={'class': 'form-control'}),
+            'tipo_documento': forms.Select(attrs={'class': 'form-control'}),
+            'numero_documento': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'telefono': forms.TextInput(attrs={'class': 'form-control'}),
+        }
+
+
